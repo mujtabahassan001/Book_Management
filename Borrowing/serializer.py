834d8cfb9c1948a0b41bb086bbdf0f6da328d93b
Mujtaba Sunny <mujtabahassaniqbal001@gmail.com>
2025-01-20 18:19:09 +0500

    yesh

--- Borrowing/serializer.py
+++ Borrowing/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Borrowing
 
+
 class BorrowingSerializer(serializers.ModelSerializer):
     borrower = serializers.CharField(source='borrower.email', read_only=True)  
 
